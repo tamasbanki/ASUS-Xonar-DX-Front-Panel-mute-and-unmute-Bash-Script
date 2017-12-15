43a6c5bc7a342e69f65328654ab3c45519510c1e
Tamás Bánki <tamas.banki@protonmail.ch>
2017-12-15 12:23:59 +0100

    Auto select soundcard
    Now automatically selects the sound card, no need to set manually. (Up to five devices)

--- alsa-fp-switch.sh
+++ alsa-fp-switch.sh
@@ -3,8 +3,8 @@ SCRIPT_VERSION=0.4.64
 
 #alsa-fp-switch.sh
 #ASUS Xonar DX Front Panel mute and unmute Bash Script
-#Build version: 1.0
-#Build date: 2017.11.01.
+#Build version: 1.1
+#Build date: 2017.12.15.
 #Made by Tamas Banki
 
 #################################################################################
@@ -23,31 +23,27 @@ SCRIPT_VERSION=0.4.64
 #You should have received a copy of the GNU General Public License
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-
-#Requires ALSA with amixer utility!
-
-#Install
-#1. Put this script file somewhere (like "/home/user/scripts/")
-#2. Open the terminal where is the script file
-#3. Make it executable with $chmod +x alsa-fp-switch.sh
-#4. Run the script with ./alsa-fp-switch.sh
-#(5.) Create a shortcut with Exec= /path/to/file/alsa-fp-switch.sh
-
-#If the script is not working try the following:
-#1. Check your sound card's number with $cat /proc/asound/cards
-#2. Change the "card_number" variable's value to that number (in my case it's "0").
 ##################################################################################
 
-card_number="0"
+input=$(arecord -l)
+
+if [[ $input == *"card 0: DX [Xonar DX]"* ]]; then
+    card_number="0"
+elif [[ $input == *"card 1: DX [Xonar DX]"* ]]; then
+    card_number="1"
+elif [[ $input == *"card 2: DX [Xonar DX]"* ]]; then
+    card_number="2"
+elif [[ $input == *"card 3: DX [Xonar DX]"* ]]; then
+    card_number="3"
+elif [[ $input == *"card 4: DX [Xonar DX]"* ]]; then
+    card_number="4"
+fi
 
 
 input=$(amixer -c "$card_number" sget "Front Panel")
 
 if [[ $input == *"Mono: Playback [off]"* ]]; then
     amixer -c "$card_number" -- sset "Front Panel" unmute
-
 elif [[ $input == *"Mono: Playback [on]"* ]]; then
     amixer -c "$card_number" -- sset "Front Panel" mute
-
 fi
